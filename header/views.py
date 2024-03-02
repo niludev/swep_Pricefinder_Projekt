f6b1872f8d79f8370d732a87e0a9239e58bd6f9d
SuperNova <nilu.shahmoradi@gmail.com>
2024-03-02 14:03:51 +0100

    Added Bootstrap

--- header/views.py
+++ header/views.py
@@ -1,8 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Header
 
 # def headerView(request):
 #     return render(request, 'home.html')
 
 
-class HeaderView(TemplateView):
+class HeaderView(ListView):
+    model = Header
     template_name = 'home.html'
