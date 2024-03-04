674d9ebf42828928ca1f8491a602f3c00f036f7c
SuperNova <nilu.shahmoradi@gmail.com>
2024-03-04 22:14:10 +0100

    Fech data

--- user_interface/views.py
+++ user_interface/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView
 from .models import Form
 from django.views import View
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
 
 # Create your views here.
 
@@ -15,6 +18,11 @@ class indexView(ListView):
     template_name = 'form.html'
 
 
+@method_decorator(csrf_exempt, name='dispatch')  # Disable CSRF token for this view
 class submitView(View):
-    def post(self, request) -> None:
-        return JsonResponse({'ok': True})
+    def post(self, request) -> JsonResponse:
+        data = json.loads(request.body)
+        # ......
+        # price = 120000
+        price = 120000
+        return JsonResponse({'price': price})
